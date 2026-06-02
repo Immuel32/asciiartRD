d309f4aca469f5305eb6075767f91e5d5836e34e
Immuel32 <151801874+Immuel32@users.noreply.github.com>
2026-06-02 14:35:04 +0100

    Refactor GenerateFont function for clarity

--- font.go
+++ font.go
@@ -3,35 +3,20 @@ package main
 func GenerateFont() map[rune][]string {
 	font := make(map[rune][]string)
 
-	for c := rune(32); c <= rune(126); c++ {
+	for c := rune(32); c <= 126; c++ {
 		lines := make([]string, 8)
 
-		if c == ' ' {
-			for i := 0; i < 8; i++ {
-				lines[i] = "        "
-			}
-			font[c] = lines
-			continue
-		}
-
 		for i := 0; i < 8; i++ {
-			line := []rune("........")
-			lines[i] = string(line)
-		}
+			row := []rune("........")
 
-		switch {
-		case c >= 'A' && c <= 'Z':
+			switch {
+			case c == ' ':
+				row = []rune("        ")
 
-			if isVowel(c) {
-				for i := 0; i < 8; i++ {
-					row := []rune(lines[i])
-					row[0], row[7] = '*', '*'
-					row[i] = '*'
-					lines[i] = string(row)
-				}
-			} else {
-				for i := 0; i < 8; i++ {
-					row := []rune(lines[i])
+			case c >= 'A' && c <= 'Z':
+				if isVowel(c) {
+					row[0], row[7], row[i] = '*', '*', '*'
+				} else {
 					for j := 0; j < 8; j++ {
 						if (i+j)%2 == 0 {
 							row[j] = '*'
@@ -39,13 +24,9 @@ func GenerateFont() map[rune][]string {
 							row[j] = ' '
 						}
 					}
-					lines[i] = string(row)
 				}
-			}
 
-		case c >= 'a' && c <= 'z':
-			for i := 0; i < 8; i++ {
-				row := []rune(lines[i])
+			case c >= 'a' && c <= 'z':
 				for j := 0; j < 8; j++ {
 					if (i+j)%2 == 1 {
 						row[j] = '*'
@@ -53,29 +34,19 @@ func GenerateFont() map[rune][]string {
 						row[j] = ' '
 					}
 				}
-				lines[i] = string(row)
-			}
 
-		case c >= '0' && c <= '9':
-
-			for i := 0; i < 8; i++ {
-				row := []rune(lines[i])
-				if i == 0 || i == 7 || i == 3 {
-					for j := 0; j < 8; j++ {
-						row[j] = '*'
-					}
+			case c >= '0' && c <= '9':
+				if i == 0 || i == 3 || i == 7 {
+					row = []rune("********")
 				} else {
 					row[0], row[7] = '*', '*'
 				}
-				lines[i] = string(row)
+
+			default:
+				row[2], row[5] = '*', '*'
 			}
 
-		default:
-			for i := 0; i < 8; i++ {
-				row := []rune(lines[i])
-				row[2], row[5] = '*', '*'
-				lines[i] = string(row)
-			}
+			lines[i] = string(row)
 		}
 
 		font[c] = lines
